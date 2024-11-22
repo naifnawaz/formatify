ad469d2e0353523608fccab1e76749da60702763
naifnawaz <naifnawaz11@gmail.com>
2024-11-22 20:24:56 +0530

    basic functionality of image producing on button click using parameter and array index && chosenOption() function completed

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+const casualLetter = [
+  "images/Screenshot(77).png",
+  "images/Screenshot_20241108_090323.png",
+];
+
 function chosenOption() {
   let selected = document.getElementById("dropdown").selectedIndex;
-  console.log(selected);
+  switch (selected) {
+    case 0:
+      generateFormat(casualLetter);
+  }
+}
+
+function generateFormat(format) {
+  const image = document.getElementById("image");
+  let index = 0;
+
+  image.src = format[index];
+  index++;
+  image.src = format[index];
 }

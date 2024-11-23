9bb79bdd674cec5c7190a0ef5b083bf2a7e64f88
naifnawaz <naifnawaz11@gmail.com>
2024-11-23 22:00:44 +0530

    made functioning next button

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
+let image = document.getElementById("image");
+const btnNext = document.getElementById("next");
+let selected = document.getElementById("dropdown").selectedIndex;
+let index = 0;
+let selectedFormat = [];
 const casualLetter = [
   "images/Screenshot(77).png",
   "images/Screenshot_20241108_090323.png",
 ];
 
 function chosenOption() {
-  let selected = document.getElementById("dropdown").selectedIndex;
-  const image = document.getElementById("image");
   image.removeAttribute("style");
   image.removeAttribute("onerror");
   switch (selected) {
     case 0:
+      selectedFormat = casualLetter;
       generateFormat(casualLetter);
+      break;
   }
 }
 
 function generateFormat(format) {
-  const image = document.getElementById("image");
-  let index = 0;
+  btnNext.addEventListener("click", next);
   image.src = format[index];
+}
+
+function next() {
+  index++;
+  console.log(index);
+  generateFormat(selectedFormat);
 }
